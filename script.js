3a7d0985fc00a7ed9698fcba41ac67705f7a013b
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-10-18 18:14:00 -0500

    latest updated

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var generateBtn = $(".generateBtn");
-//var clearBtn = $('.clearBtn');
 var addBtn = $(".addBtn");
-var inputIngredient = $(".inputIngredient")
 var ingrForm = $('#ingr-form')
 var ingUl = $('#ingUl')
 
@@ -15,33 +13,56 @@ function handleFormSubmit(event) {
         return;
     }
 
-    ingUl.append('<li id="ingredient">' + ingrItem + '<button class="clearBtn">' + "X" + '</button>' + '</li>');
+    ingUl.append('<li>' + ingrItem + '<button class="clearBtn">' + "X" + '</button>' + '</li>');
 
     $('input[name="ingrInput"]').val('')
+
+    //$().ready(function () {
+
+    $(".ingrUl li").each(function (n) {
+        $(this).attr("id", "ingr" + n);
+        console.log(ingrItem)
+
+
+    });
+    // var ingrNumber = $(this).(".ingrUl").attr("data-ingr");
+    // var input = $(this).sibling(".input").val();
+    // localStorage.setItem(ingrNumber, input);
+    localStorage.setItem('ingrInput', ingrItem);
+
+
 }
 
+
+
+
+
+window.onload = function () {
+    $('ingri').val(localStorage.getItem());
+    
+
+
+}
+
+
+
 ingrForm.on('submit', handleFormSubmit);
-ingUl.on('click', '.clearBtn', function(){
+ingUl.on('click', '.clearBtn', function () {
     $(this).parent().remove();
 })
 
 
-window.onload = function () {
-    $('#ingr1').val(localStorage.getItem(1));
-    $('#ingr2').val(localStorage.getItem(2));
-    $('#ingr3').val(localStorage.getItem(3));
-    $('#ingr4').val(localStorage.getItem(4));
-}
-generateBtn.on("click", function () {
 
-    var ingredientNumber = $(this).siblings(".allIngredients").attr("data-ingr");
-    var input = $(this).siblings(".inputIngredient").val();
+// generateBtn.on("click", function () {
 
-    localStorage.setItem(ingredientNumber, input);
+//     var ingredientNumber = $(this).siblings(".allIngredients").attr("data-ingr");
+//     var input = $(this).siblings(".inputIngredient").val();
 
-    getApi(cocktailDBSearch);
+//     localStorage.setItem(ingredientNumber, input);
 
-});
+//     getApi(cocktailDBSearch);
+
+//});
 
 // clearBtn.on('click', function () {
 //     var ingredientNumber = $(this).siblings("allIngredients").attr("data-ingr");
